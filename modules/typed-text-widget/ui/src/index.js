ac4ad47d091b1de2ce94d44fb1720a3c46711c3e
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-11-20 15:50:36 -0500

    Fix typed-text-widget cursor styles to match text

--- modules/typed-text-widget/ui/src/index.js
+++ modules/typed-text-widget/ui/src/index.js
@@ -1,20 +1,27 @@
-/* global apos, Typed */
+/* global apos, $, Typed */
 
 export default function main() {
   apos.util.widgetPlayers.typedText = {
     selector: ".typed-text-widget",
     player(element) {
-      element.innerHTML = ""; // clear previous
-      const child = document.createElement(element.dataset.widgetTag);
-      child.style.display = "inline-block";
-      element.append(child);
-      void new Typed(child, {
+      $(element).empty(); // clear previous
+      const $child = $(`<${element.dataset.widgetTag} style="display: inline-block" />`);
+      $child.appendTo(element);
+      void new Typed($child.get(0), {
         strings: element.dataset.widgetText.split(","),
         typeSpeed: 100,
         backSpeed: 20,
         smartBackspace: false,
         loop: true,
       });
+      const $cursor = $(element).find(".typed-cursor");
+      const updateCursorStyles = () => {
+        for (const prop of ["display", "font-size", "line-height", "margin-top", "margin-bottom"]) {
+          $cursor.css(prop, $child.css(prop));
+        }
+      };
+      updateCursorStyles();
+      $(window).resize(updateCursorStyles);
     },
   };
 }
